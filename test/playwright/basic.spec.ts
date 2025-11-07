d433472ed05760b5d5017927c391edf0fe8e198a
toke82 <adrian.casal.rodriguez@gmail.com>
2025-11-07 21:33:40 +0100

    finished synpress test

---
+++ test/playwright/basic.spec.ts
@@ -0,0 +1,43 @@
+import basicSetup from '../wallet-setup/basic.setup';
+import { testWithSynpress } from '@synthetixio/synpress';
+import { MetaMask, metaMaskFixtures } from '@synthetixio/synpress/playwright';
+
+
+const test = testWithSynpress(metaMaskFixtures(basicSetup));
+
+const { expect } = test;
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/TSender/);
+});
+
+test("should show the airdropfrom when connected, otherwise, not", async ({ page, 
+  context, metamaskPage, extensionId
+ }) => {
+  // Check we see "please connect wallet"
+  await page.goto('/');
+  await expect(page.getByText('Please connect')).toBeVisible();
+
+  const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId);
+  await page.getByTestId('rk-connect-button').click();
+  await page.getByTestId('rk-wallet-option-io.metamask').waitFor({
+    state: 'visible',
+    timeout: 30000
+  });
+  await page.getByTestId('rk-wallet-option-io.metamask').click();
+  await metamask.connectToDapp();
+
+  const customNetwork = {
+    name: 'Anvil',
+    rpcUrl: 'http://127.0.0.1:8545',
+    chainId: 31337,
+    symbol: 'ETH'
+  }
+  await metamask.addNetwork(customNetwork);
+
+  await expect(page.getByText('Token Address')).toBeVisible();
+})
+
